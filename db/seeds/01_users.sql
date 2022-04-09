6f36dcb9a14a54ed55075a25ffc6c22c2dcdb869
Isaiah <isaiah.iu@hotmail.com>
2022-04-09 14:57:17 +0000

    Create tables and seed fake data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,43 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password) VALUES ('Alice123','alice123@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('CatLover', 'catlover@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('DogLover', 'doglover@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('FoodLover', 'foodlover@gmail.com', 'password'),
+INSERT INTO users (username, email, password) VALUES ('LoveLover', 'lovelover@gmail.com', 'password');
+
+INSERT INTO categories (name) VALUE ('Read');
+INSERT INTO categories (name) VALUE ('Eat');
+INSERT INTO categories (name) VALUE ('Watch');
+INSERT INTO categories (name) VALUE ('Buy');
+
+INSERT INTO tasks (
+  user_id,
+  category_id,
+  name,
+  is_completed,
+  priority,
+  description,
+)
+VALUES (1,1,'Read Harry Potter', FALSE, 0, 'description');
+
+INSERT INTO tasks (
+  user_id,
+  category_id,
+  name,
+  is_completed,
+  priority,
+  description,
+  start_date,
+  end_date
+)
+VALUES (1,3,'Watch Family Guy', FALSE, 0, 'description', '2022-04-09', '2022-05-09');
+
+INSERT INTO tasks (
+  user_id,
+  category_id,
+  name,
+  is_completed,
+  priority,
+  description,
+)
+VALUES (3,2,'Eat a salad', TRUE, 2, 'description');
+
