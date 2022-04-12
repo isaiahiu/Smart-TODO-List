5f1c2b9812ed9fee3b34bbb8ea72e04dde4486fa
Isaiah <isaiah.iu@hotmail.com>
2022-04-12 18:20:06 +0000

    Submit handler for newTasks makes POST and adds to db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,32 @@
-// // Client facing scripts here
-// $(() => {
-//   $(".form-login").submit((event) => {
-//     // event.preventDefault();
-//     let $email = $(".email-input").val();
-//     let $password = $(".password-input").val();
-//     console.log($email, $password);
+// Client facing scripts here
+$(() => {
+  $(".form-login").submit((event) => {
+    // event.preventDefault();
+    let $email = $(".email-input").val();
+    let $password = $(".password-input").val();
+    console.log($email, $password);
 
-//     $.post("/entry/login", { $email, $password }).then(() => {
-//       console.log("hi");
-//     });
-//   });
+    $.post("/entry/login", { $email, $password }).then(() => {
+      console.log("hi");
+    });
+  });
 
-//   $(".add-task-form").submit((event) => {
-//     event.preventDefault();
+  $(".add-task-form").submit((event) => {
+    event.preventDefault();
+    console.log("add task form event ");
+    // $(#)
+    let $taskName = $(".add-task-text").val();
+    let $category = $("#category").val();
+    console.log("taskname is ", $taskName);
 
-//     $.post('/home/tasks', {}).then((result) => {
-//       $('.movie-list').prepend(result)
-//     })
-//   });
-// });
+    let object = { $taskName, $category };
+
+    $.post("/home/tasks", object, (result) => {
+      console.log("this is the result of ajax post : ", result);
+    });
+    // .then((result) => {
+    // console.log("ajax call result ", result);
+    // return $(".movie-list").prepend(result);
+  });
+  // });
+});
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ const { addNewTask } = require("../helperfuncs/helpfunctions");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  loadTasks()
+
+
   return res.render("index"); //pass in user object here with tasks
 });
 router.post("/tasks", (req, res) => {
-  console.log("req body is ", req.body);
-  const task = req.body.addTask;
-  const category = req.body["category-dd"];
+  console.log("req body is ", req);
+  const task = req.body["$taskName"];
+  const category = req.body["$category"];
   const id = req.session.user_id;
+  console.log("cookie is : ", req.session.user_id);
   addNewTask(id, task, category).then((result) => {
-    return res.json(result);
+    console.log("success~!");
+    res.json(result);
   });
   // res.send("hello");
 });
