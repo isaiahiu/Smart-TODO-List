00fa5e3a503ea3070f0735357b7d0ef925c872e8
Isaiah <isaiah.iu@hotmail.com>
2022-04-14 18:13:32 +0000

    Fix conflicts with class names and jquery

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/tasks", (req, res) => {
+  console.log("req is : ", req);
   const task = req.body["$taskName"];
   const id = req.session.user_id;
   catergorizer(task)
